b5b14f0fa2c5e544c779c34d0446dc1f600528ec
Alex <alexos977@yandex.ru>
2023-03-08 23:18:12 +1000

    add sql database + botrun.bat

--- bot/botrun.py
+++ bot/botrun.py
@@ -1,7 +1,19 @@
-import os
+import os, sqlite3
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(commands_prefix='!')
+
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print("Бот готов к работе")
+
+    global base, cur
+    base = sqlite3.connect('Бот.db')
+    cur = base.cursor()
+    if base:
+        print("Database connected...OK")
+
 
 bot.run(os.getenv('TOKEN'))
