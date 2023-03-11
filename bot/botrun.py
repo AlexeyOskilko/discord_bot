13ac8fe89d2f2ec7c7dc690d5da215e75121dc04
Alex <alexos977@yandex.ru>
2023-03-11 20:18:37 +1000

    add bot commands

--- bot/botrun.py
+++ bot/botrun.py
@@ -7,14 +7,30 @@ bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
-    print("Бот готов к работе")
+    pass
+# print("Бот готов к работе")
+#
+# global base, cur
+# base = sqlite3.connect('Бот.db')
+# cur = base.cursor()
+# if base:
+#     print("Database connected...OK")
 
-    global base, cur
-    base = sqlite3.connect('Бот.db')
-    cur = base.cursor()
-    if base:
-        print("Database connected...OK")
+@bot.command()
+async def test(ctx):
+    await ctx.send('Бот в онлайне!')
 
+@bot.command()
+async def инфо(ctx,arg=None):
+    author = ctx.message.author
+    if arg == None:
+        await ctx.send(f'{author.mention}\nВведите: \n!инфо общая\n!инфо команды')
+    elif arg == 'общая':
+        await ctx.send(f'{author.mention}\nЯ контролёр и слежу за порядком в чате. У тебя 3 жизни. 3 мата - БАН!')
+    elif arg == 'команды':
+        await ctx.send(f'{author.mention}\n!test - Бот онлайн?\n !статус - мои жизни и предупреждения')
+    else:
+        await ctx.send(f'{author.mention}\nТакой команды нет... Введи !команды чтобы узнать список доступных команд.')
 
 bot.run(os.getenv('TOKEN'))
 
